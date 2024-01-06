1527f6b5b4345889f974907cd16374daa9823cef
Ross Fairbanks <ross@rossfairbanks.com>
2024-01-06 21:21:41 +0100

    Improve tofu logic and upgrade versions
    Signed-off-by: Ross Fairbanks <ross@rossfairbanks.com>

--- infrastructure/equinix-metal/main.tf
+++ infrastructure/equinix-metal/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     equinix = {
       source  = "equinix/equinix"
-      version = "1.13.0"
+      version = "1.22.0"
     }
     null = {
       source = "hashicorp/null"
@@ -24,7 +24,7 @@ provider "equinix" {
 
 resource "equinix_metal_project_ssh_key" "ssh_key" {
   name       = var.cluster_name
-  project_id = var.project_id
+  project_id = var.equinix_project_id
   public_key = var.ssh_public_key
 }
 
@@ -34,9 +34,16 @@ resource "equinix_metal_device" "control_plane" {
   metro               = var.device_metro
   operating_system    = var.device_os
   billing_cycle       = var.billing_cycle
-  project_id          = var.project_id
+  project_id          = var.equinix_project_id
   depends_on          = [equinix_metal_project_ssh_key.ssh_key]
   project_ssh_key_ids = [equinix_metal_project_ssh_key.ssh_key.id]
+  
+  behavior {
+    allow_changes = [
+      "custom_data",
+      "user_data"
+    ]
+  }
 
   connection {
     user = "root"
@@ -46,7 +53,7 @@ resource "equinix_metal_device" "control_plane" {
 
   provisioner "remote-exec" {
     inline = [
-      "curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${var.k3s_version} K3S_TOKEN=${var.k3s_agent_token} sh -s - server --node-taint CriticalAddonsOnly=true:NoExecute --flannel-backend=none --disable-network-policy",
+      "curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=${var.k3s_version} K3S_TOKEN=${var.k3s_token} sh -s - server --node-taint node-role.kubernetes.io/control-plane=:NoSchedule --flannel-backend=none --disable-network-policy --disable=traefik",
       "systemctl is-active --quiet k3s.service",
     ]
   }
@@ -61,16 +68,17 @@ resource "equinix_metal_device" "worker" {
   metro               = var.device_metro
   operating_system    = var.device_os
   billing_cycle       = var.billing_cycle
-  project_id          = var.project_id
+  project_id          = var.equinix_project_id
   project_ssh_key_ids = [equinix_metal_project_ssh_key.ssh_key.id]
   depends_on          = [equinix_metal_device.control_plane]
   user_data           = <<EOF
 #!/bin/bash
-curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="${var.k3s_version}" sh -s - agent --token "${var.k3s_agent_token}" --server "https://${equinix_metal_device.control_plane.access_private_ipv4}:6443"
+curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL="${var.k3s_version}" sh -s - agent --token "${var.k3s_token}" --server "https://${equinix_metal_device.control_plane.access_private_ipv4}:6443"
 EOF
 
   behavior {
     allow_changes = [
+      "custom_data",
       "user_data"
     ]
   }
@@ -78,6 +86,9 @@ EOF
 
 resource "null_resource" "install_cilium_cni" {
   depends_on          = [equinix_metal_device.control_plane]
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 
   connection {
     user = "root"
@@ -103,8 +114,32 @@ resource "null_resource" "install_cilium_cni" {
   }
 }
 
-resource "null_resource" "bootstrap_flux" {
+resource "null_resource" "flux_env_vars" {
   depends_on          = [null_resource.install_cilium_cni]
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  connection {
+    user = "root"
+    private_key = file(var.ssh_private_key_path)
+    host = equinix_metal_device.control_plane.access_public_ipv4
+  }
+
+  provisioner "file" {
+    content = <<EOF
+GITHUB_TOKEN=${var.flux_github_token}
+KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+EOF
+    destination = "/tmp/flux_env_vars"
+  }
+}
+
+resource "null_resource" "bootstrap_flux" {
+  depends_on          = [null_resource.flux_env_vars]
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 
   connection {
     user = "root"
@@ -114,10 +149,9 @@ resource "null_resource" "bootstrap_flux" {
 
   provisioner "remote-exec" {
     inline = [
+      "export $(cat /tmp/flux_env_vars | xargs) && env && rm /tmp/flux_env_vars",
       "curl -s https://fluxcd.io/install.sh | sudo FLUX_VERSION=${var.flux_version} bash",
-      "export GITHUB_TOKEN=${var.flux_github_token}",
-      "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml",
-      "flux bootstrap github --owner=${var.flux_github_user} --repository=green-reviews-tooling --path=clusters"
+      "flux bootstrap github --owner=${var.flux_github_user} --repository=${var.flux_github_repo} --path=clusters --branch=wip-tofu-test"
     ]
   }
 }
