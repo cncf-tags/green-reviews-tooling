6884b542362d42b414b748c6483a7ab09f53714c
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-06-19 23:24:17 +0530

    added error handling
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- scripts/project-trigger.sh
+++ scripts/project-trigger.sh
@@ -3,10 +3,15 @@
 
 json_file="../projects/projects.json"
 gh_token=$GH_TOKEN
+git_ref="main"
 workflow_organization_name="cncf-tags"
 workflow_project_name="green-reviews-tooling"
 workflow_dispatcher_file_name="dispatch.yaml"
 
+if [ -z "$gh_token" ]; then
+    echo "[FATL] GH_TOKEN not set"
+    exit 20
+fi
 
 jq -c '.projects[]' "$json_file" | while read -r project; do
     proj_name=$(echo "$project" | jq -r '.name')
@@ -17,19 +22,36 @@ jq -c '.projects[]' "$json_file" | while read -r project; do
     echo "Organization: $proj_organization"
     echo "SubComponents: $sub_components"
 
-    releaseUrl="https://api.github.com/repos/${proj_organization}/${proj_name}/releases/latest"
+    release_url="https://api.github.com/repos/${proj_organization}/${proj_name}/releases/latest"
     
     
-    latest_proj_version=$(curl -fsSL -X GET $releaseUrl | jq -r '.tag_name')
+    response=$(curl -fsSL -X GET $release_url)
+    status_code=$?
+    if [ $status_code -ne 0 ]; then
+        echo "Error fetching latest release for ${proj_name} from ${release_url}. Status code: $status_code"
+        continue
+    fi
+
+    latest_proj_version=$(echo "$response" | jq -r '.tag_name')
+    if [ -z "$latest_proj_version" ]; then
+        echo "Error: Could not find the latest version for ${proj_name}"
+        continue
+    fi
     echo "Version: $latest_proj_version"
 
-
     workflow_dispatch=$(curl -fsSL -X POST \
-      -H "Accept: application/vnd.github+json" \
-      -H "Authorization: Bearer $gh_token" \
-      -H "X-GitHub-Api-Version: 2022-11-28" \
-      "https://api.github.com/repos/$workflow_organization_name/$workflow_project_name/actions/workflows/$workflow_dispatcher_file_name/dispatches" \
-      -d "{\"ref\":\"main\",\"inputs\":{\"cncf_project\":\"${proj_name}\",\"cncf_project_sub\":\"${sub_components}\",\"version\":\"${latest_proj_version}\"}}")
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $gh_token" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        "https://api.github.com/repos/$workflow_organization_name/$workflow_project_name/actions/workflows/$workflow_dispatcher_file_name/dispatches" \
+        -d "{\"ref\":\"${git_ref}\",\"inputs\":{\"cncf_project\":\"${proj_name}\",\"cncf_project_sub\":${sub_components},\"version\":\"${latest_proj_version}\"}}")
+
+    status_code=$?
+    if [ $status_code -ne 0 ]; then
+        echo "Error dispatching workflow for ${proj_name}. Status code: $status_code"
+        echo "Response: $workflow_dispatch_response"
+        continue
+    fi
 
     echo "workflow_call event [proj: $proj_name]=> $workflow_dispatch"
     echo "-----------------------------"
