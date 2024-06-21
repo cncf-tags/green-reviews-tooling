313083e7864d75b2e2bf3c3a1eb4ef79ef55780e
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-06-21 17:45:27 +0530

    added the handling of sub_components
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- scripts/project-trigger.sh
+++ scripts/project-trigger.sh
@@ -41,20 +41,41 @@ jq -c '.projects[]' "$json_file" | while read -r project; do
 
     echo "latest version: $latest_proj_version"
 
-    # TODO(dipankar): need to make the cncf_project_sub to be each individiual call for each sub-component
-    workflow_dispatch=$(curl --fail-with-body -sSL -X POST \
-        -H "Accept: application/vnd.github+json" \
-        -H "Authorization: Bearer $gh_token" \
-        -H "X-GitHub-Api-Version: 2022-11-28" \
-        "https://api.github.com/repos/$workflow_organization_name/$workflow_project_name/actions/workflows/$workflow_dispatcher_file_name/dispatches" \
-        -d "{\"ref\":\"${git_ref}\",\"inputs\":{\"cncf_project\":\"${proj_name}\",\"cncf_project_sub\":${sub_components},\"version\":\"${latest_proj_version}\"}}")
+    if [ "$sub_components" == "null" ]; then
+        echo "No sub-components for $proj_name. Performing certain task..."
+        workflow_dispatch=$(curl --fail-with-body -sSL -X POST \
+            -H "Accept: application/vnd.github+json" \
+            -H "Authorization: Bearer $gh_token" \
+            -H "X-GitHub-Api-Version: 2022-11-28" \
+            "https://api.github.com/repos/$workflow_organization_name/$workflow_project_name/actions/workflows/$workflow_dispatcher_file_name/dispatches" \
+            -d "{\"ref\":\"${git_ref}\",\"inputs\":{\"cncf_project\":\"${proj_name}\",\"cncf_project_sub\":\"\",\"version\":\"${latest_proj_version}\"}}")
 
-    status_code=$?
-    if [ $status_code -ne 0 ]; then
-        echo "dispatching workflow for ${proj_name}. Status code: $status_code"
-        echo "curl response: $workflow_dispatch"
-        continue
+        status_code=$?
+        if [ $status_code -ne 0 ]; then
+            echo "dispatching workflow for ${proj_name}. Status code: $status_code"
+            echo "curl response: $workflow_dispatch"
+            continue
+        fi
+
+        echo "workflow_call event [proj: $proj_name]=> $workflow_dispatch"
+    else
+        echo "$proj_name has sub-components. Performing tasks for each sub-component..."
+        for sub_component in $(echo "$sub_components" | jq -r '.[]'); do
+            workflow_dispatch=$(curl --fail-with-body -sSL -X POST \
+                -H "Accept: application/vnd.github+json" \
+                -H "Authorization: Bearer $gh_token" \
+                -H "X-GitHub-Api-Version: 2022-11-28" \
+                "https://api.github.com/repos/$workflow_organization_name/$workflow_project_name/actions/workflows/$workflow_dispatcher_file_name/dispatches" \
+                -d "{\"ref\":\"${git_ref}\",\"inputs\":{\"cncf_project\":\"${proj_name}\",\"cncf_project_sub\":${sub_component},\"version\":\"${latest_proj_version}\"}}")
+
+            status_code=$?
+            if [ $status_code -ne 0 ]; then
+                echo "dispatching workflow for ${proj_name}. Status code: $status_code"
+                echo "curl response: $workflow_dispatch"
+                continue
+            fi
+
+            echo "workflow_call event [proj: $proj_name]=> $workflow_dispatch"
+        done
     fi
-
-    echo "workflow_call event [proj: $proj_name]=> $workflow_dispatch"
 done
