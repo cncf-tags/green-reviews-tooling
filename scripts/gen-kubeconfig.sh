0d30d5012bf9436e61ecd7fd66c195209a9c3b73
Ross Fairbanks <ross@rossfairbanks.com>
2024-06-24 18:25:07 +0200

    docs: Create kubeconfig for pipeline
    Signed-off-by: Ross Fairbanks <ross@rossfairbanks.com>

---
+++ scripts/gen-kubeconfig.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 [pipeline|readonly]"
+  exit 1
+fi
+
+ROLE_TYPE="$1"
+
+kubectl apply -f ${ROLE_TYPE}-kubeconfig-resources.yaml
+
+USER_TOKEN_VALUE=$(kubectl -n kube-system get secret/${ROLE_TYPE}-account-token -o=go-template='{{.data.token}}' | base64 --decode)
+CURRENT_CONTEXT=$(kubectl config current-context)
+CURRENT_CLUSTER=$(kubectl config view --raw -o=go-template='{{range .contexts}}{{if eq .name "'''${CURRENT_CONTEXT}'''"}}{{ index .context "cluster" }}{{end}}{{end}}')
+CLUSTER_CA=$(kubectl config view --raw -o=go-template='{{range .clusters}}{{if eq .name "'''${CURRENT_CLUSTER}'''"}}"{{with index .cluster "certificate-authority-data" }}{{.}}{{end}}"{{ end }}{{ end }}')
+CLUSTER_SERVER=$(kubectl config view --raw -o=go-template='{{range .clusters}}{{if eq .name "'''${CURRENT_CLUSTER}'''"}}{{ .cluster.server }}{{end}}{{ end }}')
+
+cat << EOF > green-reviews-cluster-${ROLE_TYPE}-config
+apiVersion: v1
+kind: Config
+current-context: ${CURRENT_CONTEXT}
+contexts:
+- name: ${CURRENT_CONTEXT}
+  context:
+    cluster: ${CURRENT_CONTEXT}
+    user: ${ROLE_TYPE}-account
+clusters:
+- name: ${CURRENT_CONTEXT}
+  cluster:
+    certificate-authority-data: ${CLUSTER_CA}
+    server: ${CLUSTER_SERVER}
+users:
+- name: ${ROLE_TYPE}-account
+  user:
+    token: ${USER_TOKEN_VALUE}
+EOF
